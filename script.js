8a2da2c9b2f1e8ded066ef378cdf698b802d129d
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-04-10 22:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
-// your code here
+	var a = 0;
+	var b = 1;
+	if(num == 1) return a;
+	if(num == 2) return b;
+	var count = 2;
+	while(count < num){
+		var temp = a;
+		a = b;
+		b = a + temp;
+		count++;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
